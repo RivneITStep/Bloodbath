259884d2143edfd52337d1ca320a8863a6c7dab1
Ivan Zubach <vanyakage@gmail.com>
2021-03-17 15:42:58 +0200

    Start menu | rocket (updated)

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject PlayButton;
+    public GameObject ExitButton;
+    void Start()
+    {
+        PlayButton.GetComponent<Button>().onClick.AddListener(delegate { PlayOnClick(); });
+        ExitButton.GetComponent<Button>().onClick.AddListener(delegate { ExitOnClick(); });
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    void PlayOnClick()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    void ExitOnClick()
+    {
+        Application.Quit(0);
+    }
+}
